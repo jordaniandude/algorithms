fd7497a135ef0bfbe8760edb85d97dea90eaa538
thejordaniandude <thejordaniandude@users.noreply.github.com>
2015-01-27 23:11:00 -0500

    Update Spiral.java

--- Spiral.java
+++ Spiral.java
@@ -1,3 +1,4 @@
+
 public class Sprial {
 
 	public static void main(String[] args) {
@@ -8,21 +9,31 @@ public class Sprial {
 		System.out.println(str.toString());
 	}
 
-	public static StringBuffer spiral(String[][] mat) {
+	public static StringBuffer spiral(String[][] matrix) {
 
-		if (mat.length == 0 || mat[0].length == 0) {
+		if (matrix.length == 0 || matrix[0].length == 0) {
 			/* empty matrix */
 			return null;
 		}
 
-		StringBuffer str = new StringBuffer();
-		int counter = mat.length * mat[0].length;
+		// the buffer that I'll append my final entries to it
+		StringBuffer str = new StringBuffer(); 
+		
+		// I'll keep minimizing the counter after each addition to the buffer then stop at 0
+		int counter = matrix.length * matrix[0].length;  
+		
+		// the following four variables will assist in specifying the row/column I started and stopped at after
+		// the addition to the buffer, this will help adding the right value following the spiral pattern
 		int startRow = 0;
-		int endRow = mat.length - 1;
+		int endRow = matrix.length - 1;
 		int startCol = 0;
-		int endCol = mat[0].length - 1;
+		int endCol = matrix[0].length - 1;
+		
+		// this is variable that will specify to move either horizontally or vertically
 		boolean moveCol = true;
+		// this is variable that will specify to move either left-to-right or right-to-left when the moveCol is horizontal (true)
 		boolean leftToRight = true;
+		// this is variable that will specify to move either up-to-down or down-to-up when the moveCol is vertical (false)
 		boolean upDown = true;
 
 		// looping through the entire matrix
@@ -32,21 +43,23 @@ public class Sprial {
 
 				if (leftToRight) {
 
-					/* printing entire row left to right */
+					// moving horizontally by printing the entire row left to right 
 					for (int i = startCol; i <= endCol; i++) {
-						str.append(mat[startRow][i]+" ");
+						str.append(matrix[startRow][i]+" ");
 						counter--;
 					}
+					//the next time I'll move horizontally, it's going to be right to left
 					leftToRight = false;
 					moveCol = false;
 					startRow++;
 				} else {
 
-					/* printing entire row right to left */
+					// moving horizontally by printing the entire row right to left
 					for (int i = endCol; i >= startCol; i--) {
-						str.append(mat[endRow][i]+" ");
+						str.append(matrix[endRow][i]+" ");
 						counter--;
 					}
+					//the next time I'll move horizontally, it's going to be left to right
 					leftToRight = true;
 					moveCol = false;
 					endRow--;
@@ -54,19 +67,21 @@ public class Sprial {
 			} else {
 				if (upDown) {
 
-					/* printing column up down */
+					// moving vertically by printing the column up-to-down
 					for (int i = startRow; i <= endRow; i++) {
-						str.append(mat[i][endCol]+" ");
+						str.append(matrix[i][endCol]+" ");
 						counter--;
 					}
+					
+					//the next time I'll move vertically, it's going to be down-to-up
 					upDown = false;
 					moveCol = true;
 					endCol--;
 				} else {
 
-					/* printing entire col down up */
+					// moving vertically by printing the column down-to-up
 					for (int i = endRow; i >= startRow; i--) {
-						str.append(mat[i][startCol]+" ");
+						str.append(matrix[i][startCol]+" ");
 						counter--;
 					}
 					upDown = true;
