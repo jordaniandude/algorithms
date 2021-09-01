53eca6041c8bd4fd9f13073067ceaaf56c6308e4
Tariq Elouzeh <9696908+jordaniandude@users.noreply.github.com>
2021-08-31 22:23:48 -0400

    Update

--- Trees.java
+++ Trees.java
@@ -44,4 +44,31 @@ class Trees {
     }
   
   
+  /*
+  Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+  */
+      public TreeNode sortedArrayToBST(int[] nums) {
+        
+        return helper(nums, 0, nums.length-1);
+    }
+    public TreeNode helper(int[] nums, int left, int right) {
+        
+        
+        if (left > right) {
+            return null;
+        }
+            int mid = left + (right - left) / 2;
+            TreeNode node = new TreeNode(nums[mid]);
+            node.left = helper(nums, left,  mid - 1);
+            node.right = helper(nums, mid+1, right);
+            return node;
+        
+        
+    }
+  
+  
+  
+  
 }
